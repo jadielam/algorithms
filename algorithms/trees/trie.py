2b7bbc0c8a2a5d877a112f3cfc1bfa5714d2ecfa
Jadiel deArmas <Jadiel.X.De.Armas.-ND@disney.com>
2020-08-14 14:03:12 -0400

    Fixed bug of algorithms

--- algorithms/trees/trie.py
+++ algorithms/trees/trie.py
@@ -4,9 +4,9 @@ class TrieNode:
     '''
     The root trie will have ch = None and terminal = False
     '''
-    def __init__(self, ch, terminal = False):
+    def __init__(self, ch):
         self.ch = ch
-        self.terminal = terminal
+        self.word = None
         self.children : Dict[str, TrieNode] = {}
 
 def add_word(trie_node: TrieNode, word: str):
@@ -22,7 +22,7 @@ def add_word(trie_node: TrieNode, word: str):
         else:
             ch_trie_node = current_node.children[ch]
             current_node = ch_trie_node
-    current_node.terminal = True
+    current_node.word = word
 
 def find_node(trie_node: TrieNode, prefix: str) -> TrieNode:
     '''
@@ -31,7 +31,7 @@ def find_node(trie_node: TrieNode, prefix: str) -> TrieNode:
     returns None
     '''
     current_node = trie_node
-    for ch in word:
+    for ch in prefix:
         if not ch in current_node.children:
             return None
         current_node = current_node.children[ch]
@@ -42,7 +42,7 @@ def search_word(trie_node: TrieNode, word: str) -> bool:
     Returns true if word was found in trie, otherwise returns false
     '''
     final_node = find_node(trie_node, word)
-    return final_node.terminal if final_node.terminal else None
+    return True if final_node is not None and final_node.word is not None else False
 
 def prefix_search(trie_node: TrieNode, prefix: str) -> List[str]:
     '''
@@ -55,5 +55,12 @@ def prefix_search(trie_node: TrieNode, prefix: str) -> List[str]:
         return to_return
     
     # Do BFS on nodes, appending words as I find them as terminals
-
+    queue = [final_node]
+    while len(queue) > 0:
+        node = queue.pop(0)
+        if node.word is not None:
+            to_return.append(node.word)
+        for child_node in node.children.values():
+            queue.append(child_node)
     
+    return to_return
