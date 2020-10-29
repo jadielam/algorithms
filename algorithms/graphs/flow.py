e684e4994a521ad3c70b601646810f4eab2e42a8
Jadiel deArmas <Jadiel.X.De.Armas.-ND@disney.com>
2020-10-29 13:18:53 -0400

    Finished the maxflow-mincut algorithm

---
+++ algorithms/graphs/flow.py
@@ -0,0 +1,150 @@
+from typing import Dict, List, Hashable, Tuple
+
+def compute_residual(G: Dict[Hashable, List[Hashable]],
+                    c: Dict[Tuple[Hashable, Hashable], float],
+                    f: Dict[Tuple[Hashable, Hashabe], float]):
+    '''
+    For each edge (u, v) in G:
+    1. if f[u, v] < c[u, v], create forward edge (u, v) with
+        cr[u, v] = c[u, v] - f[u, v]
+    2. if f[u, v] > 0, create backward edge (v, u) with cr[v, u] = f[u, v]
+
+    - Arguments:
+        - G: adjacency list of original graph
+        - c: dictionary of capacities of edges
+        - f: dictionary of flows in edges
+    
+    - Returns:
+        - Gr (Dict[Hashable, List[Hashable]]): adjacency list of residual graph
+        - edge_type (Dict[Tuple[Hashable, Hashable], str]): dictionary that contains edge type for each edge
+            in the residual graph.
+        - cr (Dict[Tuple[Hashable, Hashable], float]): capacity of each edge in the residual graph.
+    '''                    
+    Gr = {}
+    edge_type = {}
+    cr = {}
+
+    for u, adj in enumerate(G):
+        for v in adj:
+            if f[u, v] < c[u, v]:
+                if u not in Gr:
+                    Gr[u] = []
+                Gr[u].append(v)
+                edge_type[u, v] = 'forward'
+                cr[u, v] = c[u, v] - f[u, v]
+            if f[u, v] > 0:
+                if v not in Gr:
+                    Gr[v] = []
+                Gr[v].append(u)
+                edge_type[v, u] = 'backward'
+                c[v, u] = f[u, v]
+
+    return Gr, edge_type, cr
+
+def get_shortest_path(Gr: Dict[Hashable, List[Hashable]], s: Hashable, t: Hashable) -> List[Hashable]:
+    '''
+    - Arguments:
+        - Gr: Residual graph
+        - s: source node id
+        - t: sink node id
+    
+    - Returns:
+        - p: List of nodes that make an s-t path in Gr.
+            Returns None if no such path exists
+    '''
+    queue = [s]
+    parent: Dict[Hashable, Hashable] = dict()
+    visited = set()
+    while queue:
+        u = queue.pop(0)
+        if u == t:
+            break
+        visited.add(u)
+        if u in Gr:
+            for v in Gr[u]:
+                if not v in visited:
+                    parent[v] = u
+                    queue.append(v)
+    
+    if u != t:
+        return None
+    current_node = t
+    path = [current_node]
+    while current_node != s:
+        current_node = parent[current_node]
+        path.insert(0, current_node)
+    return path
+
+def augment(f: Dict[Tuple[Hashable, Hashable], float], 
+            p: List[Hashable], 
+            edge_type_d: Dict[Tuple[Hashable, Hashable], str], 
+            cr: Dict[Tuple[Hashable, Hashable], float]):
+    '''
+    It does not return anything since it modifies flow f in place.
+
+    - Arguments:
+        - f: flow
+        - p: s-t path in residual graph
+        - edge_type_d: dictionary that contains edge types
+        - cr: dictionary of capacity of edges in residual graph
+    - Returns: 
+        None
+    '''
+    #1. Find bottleneck value
+    bottleneck_value = float('inf')
+    for idx, v in enumerate(p[1:]):
+        u = p[idx - 1]
+        c_edge = cr[u, v]
+        if c_edge < bottleneck_value:
+            bottleneck_value = c_edge
+    
+    for idx, v in enumerate(p[1:]):
+        u = p[idx - 1]
+        edge_type = edge_type_d[u, v]
+        if edge_type == 'forward':
+            f[u, v] += bottleneck_value
+        else:
+            f[v, u] -= bottleneck_value
+
+def edmonds_karp(G: Dict[Hashable, List[Hashable]],
+                c: Dict[Tuple[Hashable, Hashable], float],
+                s: Hashable,
+                t: Hashable) -> Dict[Tuple[Hashable, Hashable], float]:
+    '''
+    This is the Edmonds-Karp algorithm that runs in O(VE^2). THe algorithm
+    proceeeds like this:
+    - Initialize flow to 0
+    - Compute residual graph Gr of G
+    - While there is an augmenting path p in Gr:
+        - Pick path with the least number of edges
+        - Augment flow across p
+        - Update f
+        - Recompute Gf
+    - Return f
+
+    - Arguments:
+        - G: The original graph defined as an adjacency list
+        - c: Dictionary containing the capacity of each node in 
+            the graph.
+        - s: Source node
+        - t: Target (sink node)
+    - Returns:
+        - f (Dict[Tuple[Hashable, Hashable], float]): The optimal flow from s to t
+        that satisfies the capacity constraints.
+    '''
+
+    #1. Create f and set it to zero
+    f: Dict[Tuple[Hashable, Hashable], float] = {}
+    for u, adj in enumerate(G):
+        for v in adj:
+            f[u, v] = 0
+    
+    #2. Compute residual graph Gf
+    Gr, edge_type, cr = compute_residual(G, c, f)
+
+    p = get_shortest_path(Gr, s, t)
+    while p is not None:
+        augment(f, p)
+        Gr, edge_type, cr = compute_residual(G, c, f)
+        p = get_shortest_path(Gr, s, t)
+    return f
--- algorithms/trees/rb_tree.py
+++ algorithms/trees/rb_tree.py
@@ -1,29 +1,51 @@
-from typing import Iterator, Hashable, List
+from typing import Hashable
 from enum import Enum
 
 class Color(Enum):
     RED = 1
     BLACK = 2
 
-NilNode = RBNode(
-    None,
-    None
-)
+class NilNode:
+    def __init__(self, parent = NIL):
+        self.color : Color = Color.BLACK
+        self.parent : RBNode = parent
 
-class RBNode:
+NIL = NilNode()
+
+class RBNode(object):
     def __init__(self, value: Hashable, data = None):
         self.value = value
         self.data = data
-        self.parent : RBNode = NilNode
+        self.parent : RBNode= NIL
         self.color : Color = Color.BLACK
-        self.left : RBNode = NilNode
-        self.right = RBNode = NilNode
-
-
+        self.left : RBNode= NIL
+        self.right : RBNode = NIL
 
 def left_rotate(x: RBNode):
     y = x.right
-    x.right = 
+    x.right = y.left
+    y.parent = x.parent
+    if x.parent is not NIL:
+        if x is x.parent.left:
+            x.parent.left = y
+        elif x is x.parent.right:
+            x.parent.right = y
+    y.left = x
+    x.parent = y
+    return x
+
+def right_rotate(y: RBNode):
+    x = y.left
+    y.left = x.right
+    x.parent = y.parent
+    if y.parent is not NIL:
+        if y is y.parent.left:
+            y.parent.left = x
+        elif y is y.parent.right:
+            y.parent.right = x
+    x.right = y
+    y.parent = x
+    return y
 
 def tree_insert(root: RBNode, z: RBNode) -> RBNode:
     '''
