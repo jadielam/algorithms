20e6801d75664d5fdd892a05d115a36d45d8c86a
Jadiel de Armas <jadielam@gmail.com>
2020-11-22 11:16:09 -0500

    Finished the combinatorics generators code

--- algorithms/combinatorics/generators.py
+++ algorithms/combinatorics/generators.py
@@ -48,6 +48,35 @@ def combinations_from_product(l: list, k: int):
         to_return.append(c_entry)
     return to_return
 
+def combinations_from_permutations(l: list, k: int):
+    '''
+    Given a list l of elements, returns all possible combinations of 
+    k elements from the list. k must be less than or equal to the length of
+    the list.
+    If n is the length of the list, the number of possible permutations is
+    equal to n!/((n-k)!n!)
+    '''
+    n = len(l)
+    to_return = []
+    for indices in permutations(list(range(n)), k):
+        if sorted(indices) == list(indices):
+            to_return.append(indices)
+    return to_return
+
+def combinations_with_replacement_from_product(l: list, k: int):
+    '''
+    Given a list l of elements, returns all possible combinations of r elements
+    from the list. r must be less than or equal to the length of the list.
+    If n is the length of the list, the number of possible combinatinos with replacement is
+    equal to (n + k - 1)!/(k!(n + k - 1 - k)!)
+    '''
+    n = len(l)
+    to_return = []
+    for indices in product(list(range(n)), k):
+        if sorted(indices) == list(indices):
+            to_return.append(indices)
+    return to_return
+
 def permutations(iterable, r = None):
     '''
     This is close to the official Python itertools library implementation.
@@ -61,27 +90,73 @@ def permutations(iterable, r = None):
         return
     indices = list(range(n))
     cycles = list(range(n, n-r, -1))
-    yield tuple(pool[i] for i in indices[:r])
+    to_return = []
+    to_return.append(tuple(pool[i] for i in indices[:r]))
     while n:
         for i in reversed(range(r)):
             cycles[i] -= 1
             if cycles[i] == 0:
+                # Send number to the back of the line
                 indices[i:] = indices[i+1:] + indices[i:i+1]
+                # Restart count again
                 cycles[i] = n - i
             else:
                 j = cycles[i]
                 indices[i], indices[-j] = indices[-j], indices[i]
-                yield tuple(pool[i] for i in indices[:r])
+                to_return.append(tuple(pool[i] for i in indices[:r]))
                 break
         else:
-            return
+            return to_return
 
 def combinations(l: list, k: int):
     '''
     Given a list l of elements, returns all possible combinations of 
-    k elements from the list. k must be less than or equal to the length of
+    r elements from the list. r must be less than or equal to the length of
     the list.
     If n is the length of the list, the number of possible permutations is
     equal to n!/((n-k)!n!)
     '''
-    pass
+    pool = tuple(l)
+    n = len(pool)
+    if k > n:
+        return
+    indices = list(range(k))
+    to_return = []
+    to_return.append(tuple(pool[i] for i in indices))
+    while True:
+        for i in reversed(range(k)):
+            if indices[i] != i + n - k:
+                break
+        else:
+            return to_return
+        indices[i] += 1
+        # A guarantee that we don't repeat elements
+        # Do we need it?
+        for j in range(i + 1, k):
+            indices[j] = indices[j - 1] + 1
+        to_return.append(tuple(pool[i] for i in indices))
+    return to_return
+
+def combinations_with_replacement(l: list, k: int):
+    '''
+    Given a list l of elements, returns all possible combinations of r elements
+    from the list. r must be less than or equal to the length of the list.
+    If n is the length of the list, the number of possible combinatinos with replacement is
+    equal to (n + k - 1)!/(k!(n + k - 1 - k)!)
+    '''
+    # combinations_with_replacement('ABC', 2) --> AA AB AC BB BC CC
+    pool = tuple(l)
+    n = len(pool)
+    if not n and k:
+        return
+    indices = [0] * k
+    to_return = []
+    to_return.append(tuple(pool[i] for i in indices))
+    while True:
+        for i in reversed(range(k)):
+            if indices[i] != n - 1:
+                break
+        else:
+            return to_return
+        indices[i:] = [indices[i] + 1] * (k - i)
+        to_return.append(tuple(pool[i] for i in indices))
