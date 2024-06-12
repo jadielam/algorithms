1557d00d4f34793cf5a50d41a2516808eee45c37
jdearmas <jadiel_dearmas@intuit.com>
2024-06-12 12:13:57 -0400

    Fixed quicksort comment

--- algorithms/sorting/quicksort.py
+++ algorithms/sorting/quicksort.py
@@ -3,7 +3,7 @@ import random
 def randomized_partition(a: list, p, r) -> int:
     '''
     Given list `a` and indexes `p` and `r` (both inclusive), shuffles elements in a. it returns index
-    `q` such that all elements a[:q] are less than or equal to q and all elements a[q:] are greater
+    `q` such that all elements a[:q] are less than or equal to a[q] and all elements a[q+1:] are greater
     than `q`.
     
     - Arguments:
