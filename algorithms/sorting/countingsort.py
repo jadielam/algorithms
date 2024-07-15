063009f3dd2aeb6dcbcefcb1d7715cdc7b7885c5
jdearmas <jadiel_dearmas@intuit.com>
2024-07-15 12:27:16 -0400

    Added radix sort algorithm

--- algorithms/sorting/countingsort.py
+++ algorithms/sorting/countingsort.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 def counting_sort(A: List[int]) -> List[int]:
     """
@@ -38,7 +38,8 @@ def counting_sort(A: List[int]) -> List[int]:
 def split_int_to_digits(a: int) -> List[int]:
     """
     Returns the digits of a number as a list of integers, and
-    in reverse order of their appearance in the number.
+    in reverse order of their appearance in the number, meaning that
+    if number is 123, the result will be [3, 2, 1]
     """
     if a == 0:
         return [0]
@@ -58,12 +59,52 @@ def split_to_digits(A: List[int]) -> List[List[int]]:
     """
     return [split_int_to_digits(a) for a in A]
 
+def counting_sort_for_radix_sort(
+        A: List[Tuple[int, List[int]]], pos: int
+    ) -> List[Tuple[int, List[int]]]:
+    """
+    The counting_sort for radix sort follows the same
+    strategy as the normal counting sort, just that it
+    knows that number of unique entries is 10 (digits from 0 to 9).
+    And also, the input includes the original numbers in the list.
+
+    Arguments:
+    - A: The list of numbers to sort. The first entry in the tuple
+    is the number. The second entry is the number split into digits,
+    in reverse order, that is, if number is 123, the second entry is
+    [3, 2, 1]
+    - pos: The position of the digit to sort by. The least significant
+    digit is at position 0, the second least significant digit is at
+    position 1, and so on.  If a number does not have a digit at the
+    position we are sorting, we consider it to be 0.
+    """
+    C = [0] * 10
+    B = [None] * len(A)
+
+    for num, digits in A:
+        digit_val = 0 if pos >= len(digits) else digits[pos]
+        C[digit_val] += 1
+    
+    for i in range(1, len(C)):
+        C[i] += C[i - 1]
+
+    for i in range(len(A) - 1, -1, -1):
+        num, digits = A[i]
+        digit_val = 0 if pos >= len(digits) else digits[pos]
+        B[C[digit_val] - 1] = (num, digits)
+        C[digit_val] -= 1
+
+    return B
+        
 
 def radix_sort(A: List[int]) -> List[int]:
+    if not A:
+        return A
     splitted_list = split_to_digits(A)
     max_nb_digits = max([len(a) for a in splitted_list])
-    
-    # TODO: Continue working here.
+    input_to_sort = list(zip(A, splitted_list))
+
     for i in range(max_nb_digits):
-        # Totally doable
-        pass
+        input_to_sort = counting_sort_for_radix_sort(input_to_sort, i)
+    return [num for num, _ in input_to_sort]    
+    
--- tests/algorithms/sorting/countingsort_test.py
+++ tests/algorithms/sorting/countingsort_test.py
@@ -1,4 +1,5 @@
 from algorithms.sorting.countingsort import split_int_to_digits
+from algorithms.sorting.countingsort import radix_sort
 
 def test_split_int_to_digits():
     assert split_int_to_digits(0) == [0]
@@ -7,3 +8,19 @@ def test_split_int_to_digits():
     assert split_int_to_digits(123) == [3, 2, 1]
     assert split_int_to_digits(1234567890) == [0, 9, 8, 7, 6, 5, 4, 3, 2, 1]
     assert split_int_to_digits(12345678901234567890) == [0, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+def test_sort_empty_list():
+    assert radix_sort([]) == []
+
+def test_sort_one_item_list():
+    assert radix_sort([1]) == [1]
+
+def test_sort_inverse_list():
+    assert radix_sort([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5]
+
+def test_sort_long_random_list():
+    assert radix_sort([5, 4, 3, 2, 1, 9, 8, 7, 6, 0]) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def test_sort_multiple_digits_list():
+    to_sort = [123, 456, 789, 101, 202, 303, 404, 505, 606, 707, 808, 909]
+    assert radix_sort(to_sort) == sorted(to_sort)
