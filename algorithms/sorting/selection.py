cf48bf6f9fca6443dbb431f212fd976861b210fb
jdearmas <jadiel_dearmas@intuit.com>
2024-07-25 15:36:34 -0400

    Added runtime explanation for selection on linear time

--- algorithms/sorting/selection.py
+++ algorithms/sorting/selection.py
@@ -3,6 +3,15 @@ from .quicksort import randomized_partition
 
 def select_k(a, p, r, i):
     """
+    This algorithm runs in linear time.  The reason is this one:
+    n + n/2 + n/4 + n/8 + ... 1 is approximately equal to 2n.
+
+    In order to show the claim above, see the formula:
+    sum from 0 to n - 1 of a(r^k) is equal to a[(1 - r^n)/(1 - r)]
+
+    This is the same formula we use to compute 2^0 + ... 2^n = 2^(n+1) - 1
+    But in this case, we use it in this way: a = n and r = (1/2)
+    
     Arguments:
     - a: array
     - p: starting index inclusive
